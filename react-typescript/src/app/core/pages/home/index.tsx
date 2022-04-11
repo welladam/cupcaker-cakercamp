c4d2602c117eaf5fdafab3f7e704b2936e030146
felpsmars <mfelipeluiz99@gmail.com>
2022-04-11 15:39:37 -0300

    CUP-12_create-home-news

--- react-typescript/src/app/core/pages/home/index.tsx
+++ react-typescript/src/app/core/pages/home/index.tsx
@@ -1,18 +1,27 @@
 import React from 'react'
 
-import { Tag } from 'components/atoms'
-import { LabelGames } from 'components/atoms'
+import moment from 'moment'
+import 'moment/locale/pt-br'
+
+import { HomeNews } from 'components/molecules'
+
+moment.locale('pt-br')
 
 /* import styles from './styles.module.scss'*/
 
 const Home = (): JSX.Element => {
   return (
     <main>
-      <header>
-        <Tag label="PC" className="PC" />
-        
-        <LabelGames />
-      </header>
+      <HomeNews
+        author="Billy Butcher,"
+        title="Title: Lorem Ipsum Lorem Ips"
+        description="Description: Lorem Ipsum Lorem "
+        comments="44"
+        label="PS4"
+        stylesTag="PS4"
+        time={'2022-04-06T09:00:46.272Z'}
+        srcIMG="https://static.gamevicio.com/imagens_up/big/71/sonic-the-hedgehog-2-gerou-us-25-5-milhoes-em-seu-final-de-semana-internacional-070121.jpg"
+      />
     </main>
   )
 }
--- react-typescript/src/components/atoms/tag/index.tsx
+++ react-typescript/src/components/atoms/tag/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
 
+import classNames from 'classnames'
+
 import styles from './styles.module.scss'
 
 interface ITagsProps {
   label: string
-  className: string
+  stylesTag: string
 }
 
 const Tag = (props: ITagsProps): JSX.Element => {
-  return <div className={styles[props.className]}>{props.label}</div>
+  return (
+    <div className={classNames(styles.container, styles[props.stylesTag])}>
+      {props.label}
+    </div>
+  )
 }
 
 export { Tag }
